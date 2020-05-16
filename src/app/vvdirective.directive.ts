ae320c130b6a5d4446d852dcfac6e019acb5d8b2
unknown <vvauban@gmail.com>
2020-05-16 16:45:14 +0200

    Fix by using a directive with an input

--- src/app/vvdirective.directive.ts
+++ src/app/vvdirective.directive.ts
@@ -1,14 +1,24 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
 
 @Directive({
-  selector: '.vvdirective'
+  selector: '[vvdirective]'
 })
 export class VVdirectiveDirective {
-
-  constructor(private elRef: ElementRef) {}
-	ngOnInit() {
-    	this.elRef.nativeElement.style.backgroundColor = "gray";
-      this.elRef.nativeElement.style.color = "navy";
-      console.log("vv directive works!!")
-  	}
+  @Input('vvdirective') params: string;
+  constructor(
+    private elRef: ElementRef,
+    private renderer: Renderer2) { }
+  ngOnInit() {
+    if (this.params === 'vvstyle number two') {
+      console.log("in the first condition for other style number two")
+      console.log(this.params)
+      this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'black');
+      this.renderer.setStyle(this.elRef.nativeElement, 'color', 'yellow');
+    } else {
+      console.log("in the default condition for default style")
+      console.log(this.params)
+      this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'gray');
+      this.renderer.setStyle(this.elRef.nativeElement, 'color', 'navy');
+    }
+  }
 }
